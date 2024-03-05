4d8c5253b637f8b234cc38e81f0f7457a1eec717
Varunpro16 <91467967+Varunpro16@users.noreply.github.com>
2024-03-05 13:06:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 const dbconnect = async() => {
   try{
     
-      const connect = await mongoose.connect('mongodb://localhost:27017/vijay')
+      const connect = await mongoose.connect('mongodb://root:example@mongodb:27018/vijaybala?authMechanism=DEFAULT&authSource=admin')
       console.log("DB connected!!!");
       console.log("DB Name: ",connect.connection.name);
 
